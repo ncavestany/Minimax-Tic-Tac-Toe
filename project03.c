c4b2bfa78624c0587d9477b527dde49199a4ff07
Noah Cavestany <ncavestany@vlab12.cs.usfca.edu>
2022-10-11 18:19:21 -0700

    finished up until defensive

--- project03.c
+++ project03.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<stdbool.h>  
+#include <stdbool.h>  
+#include <sys/param.h>
 
 typedef char board_t[3][3];
 
@@ -20,6 +21,16 @@ void printBoard(board_t board) { // Prints the board with proper formatting.
 			printf("---+---+---\n");
 		 }
     
+	}
+}
+
+void initializeBoard(board_t board) {
+	int row;
+	int col;
+	for (row = 0; row < 3; row++) {
+		for (col = 0; col < 3; col++) {
+			board[row][col] = '_';
+		}
 	}
 }
 
@@ -57,7 +68,6 @@ bool checkRow(board_t board, char player) { // Check each row and see if any row
 	int row;
 	for (row = 0; row < 3; row++) {
 		if (board[row][0] == player  && board[row][1] == player &&  board[row][2] == player) {
-			printf("%c wins\n", player);
 			return true;
 		}
 	}
@@ -68,7 +78,6 @@ bool checkColumn(board_t board, char player) { // Scan through each column to ch
 	int column;
 	for (column = 0; column < 3; column++) {
 		if (board[0][column] == player && board[1][column] == player &&  board[2][column] == player) {
-			printf("%c wins\n", player);
 			return true;	
 		}	
 	}
@@ -76,13 +85,11 @@ bool checkColumn(board_t board, char player) { // Scan through each column to ch
 }
 
 bool checkDiagonal(board_t board, char player) {
-	if (board[0][0] == player && board[1][1] == player && board[2][2] == player) { // Check diagonal right (top left to bottom right) victory		
-		printf("%c wins\n", player);
+	if (board[0][0] == player && board[1][1] == player && board[2][2] == player) { // Check diagonal right (top left to bottom right) victory				
 		return true;
 	}
 	else if (board[0][2] == player && board[1][1] == player && board[2][0] == player) { // Check diagonal left
-			printf("%c wins\n", player);
-			return true;
+		return true;
 	}
 	return false;
 }
@@ -101,39 +108,140 @@ bool checkOWin(board_t board, char player2) {
 	return false;
 }
 
+bool checkDraw(board_t board) {
+	if ((checkXWin(board, 'X') == false || checkOWin(board, 'O') == false) && isFull(board)) {
+	    return true;
+	}
+	return false;
+}
+
+int minimax(board_t board, char player, int depth) {
+	int score;
+	int row;
+	int col;
+	if (checkXWin(board, player)) {
+		score = 10 - depth;
+		return score;
+	}
+	if (checkOWin(board, player)) {
+		score = -10 + depth;
+		return score;
+	}
+	if (checkDraw(board)) {
+		score = 0;
+		return score;
+	}
+
+	if (player == 'X') {
+		score = -1000;
+		for (row = 0; row < 3; row++) {
+			 for (col = 0; col < 3; col++) {
+		        if (board[row][col] == '_') {
+		        	board[row][col] = player; // make move
+		        	score = MAX(score, minimax(board, 'O', depth + 1)); // Returns the highest score
+		        	board[row][col] = '_'; //unmake move
+		        }
+		  }
+		}
+	}
+	else {
+		score = 1000;
+		for (row = 0; row < 3; row++) {
+			for (col = 0; col < 3; col++) {
+				if (board[row][col] == '_') {
+					board[row][col] = player; // make move
+					score = MIN(score, minimax(board, 'X', depth + 1)); // Returns the lowest score
+					board[row][col] = '_'; // unmake move
+				}
+			}
+		}
+	}
+	return score;
+}
+
+void find_best_move(board_t board, char player, int *pr, int *pc) {
+	int score;
+	int best = 0;
+	int row;
+	int col;
+			
+	for (row = 0; row < 3; row++) {
+		for (col = 0; col < 3; col++) {
+			if (board[row][col] == '_') {
+				board[row][col] = player; // Trial move
+				if (player == 'X') { // If the current player is X, run the trial moves for O (and vice versa).
+					score = minimax(board, 'O', 0);
+				} 
+				else {
+					score = minimax(board, 'X', 0);
+				}
+				board[row][col] = '_'; // Undo the move.
+				if (score > best && player == 'O') { // Update the current best score and best move.
+					best = score;
+					*pr = row;
+					*pc = col;
+				}
+				else if (score < best && player == 'X') {
+					best = score;
+					*pr = row;
+					*pc = col;	
+				}
+			}
+		}		
+	}
+}
+
+bool terminal_state(board_t board) {
+	if (checkXWin(board, 'X') || checkOWin(board, 'O') || checkDraw(board)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 
 int main(int argc, char **argv) {
-    if (argc != 10) {
-        printf("invalid args\n");
-        exit(-1);
-    }
-
     board_t board;
+    initializeBoard(board);
 
     int i = 1; // The current index of the user input.
     int row;
     int col;
-    char player1 = 'X';
-    char player2 = 'O';
-    
-	// Populating the board based on user input.
-    for (row = 0; row < 3; row++) {
-        for (col = 0; col < 3; col++) {
-            board[row][col] = argv[i][0];
-            i++;
-        }
-    }
 
-    printBoard(board);
-    if (ifEmpty(board)) {
-    	exit(0);
-    }
-
-	
-    if ((checkXWin(board, player1) == false || checkOWin(board, player2) == false) && isFull(board)) {
-    	printf("draw\n");
-    }
-  	
     
+	if (argc == 1) {
+		while(!terminal_state(board)) {
+			printf("X: ");
+			fscanf(stdin, "%d %d", &row, &col);
+			if (board[row][col] != 'X' || board[row][col] != 'O') {
+				board[row][col] = 'X';
+				if (terminal_state(board)) {
+					break;
+			}
+				find_best_move(board, 'O', &row, &col);
+				board[row][col] = 'O';
+				printf("O: %d %d\n", row, col);
+			}
+			else {
+				printf("Illegal move\n");
+			}
+		}
+		
+	}  
+	else {	// Populating the board based on user input if there are any arguments.
+		for (row = 0; row < 3; row++) {
+			 for (col = 0; col < 3; col++) {
+		        board[row][col] = argv[i][0];
+		        i++;
+		   }
+		}
+		row = 0;
+		col = 0;
+		printBoard(board);
+		find_best_move(board, 'O', &row, &col);
+		printf("O: %d %d\n", row, col);
+	}
+		
     return 0;
 }
